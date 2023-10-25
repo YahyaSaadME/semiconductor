2572ff5e1a7408ea3fdd7dcc24fc1263b844b416
Yahya saad <yahyasaad.me@gmail.com>
2023-10-25 14:53:48 +0530

    I

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require("express")
-const cors = require('cors')
 const app = express()
 app.use(express.json())
-app.use(cors())
 
 app.get("/",(req,res)=>{
     res.json({msg:"Server is up and running"})
