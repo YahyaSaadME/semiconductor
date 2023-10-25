713f79d007b2b1ac135edebb8862229b111e6c73
Yahya saad <yahyasaad.me@gmail.com>
2023-10-25 14:36:17 +0530

    I

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require("express")
 const app = express()
+app.use(express.json())
 
 app.get("/",(req,res)=>{
-    res.send("Done")
+    res.json({msg:"Server is up and running"})
 })
+
+app.get("/temperature",(req,res)=>{
+    res.json({Temperature:20})
+})
+
 
 app.listen(5000)
